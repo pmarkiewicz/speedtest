bfe5e7a06d93a1211c8068922206466efcdba44f
Piotr Markiewicz <piotr.markiewicz@gmail.com>
2016-12-14 22:58:11 +0100

    remote address added as param for quick test

--- quicktest.py
+++ quicktest.py
@@ -1,10 +1,28 @@
+import argparse
 import redislite
 from redis_collections import List
 
 import processor
 
+ipaddress = '192.168.10.150'
+port = '8002'
+
 if __name__ == "__main__":
-    rc = redislite.StrictRedis(serverconfig={'slaveof': 'localhost 8002'})
+    parser = argparse.ArgumentParser(description="Speedtest quicktest")
+    parser.add_argument('-a', '--address', help='Remote IP address')
+    parser.add_argument('-p', '--port', help='Remote port')
+
+    args = parser.parse_args()
+
+    if args.address:
+        ipaddress = args.address
+
+    if args.port:
+        port = args.port
+
+    slave = '{} {}'.format(ipaddress, port)
+    print slave
+    rc = redislite.StrictRedis(serverconfig={'slaveof': slave})
     lst = List(redis=rc, key='speed')
 
     print "Keys in redis: ", rc.keys()

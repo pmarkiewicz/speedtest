50d3f6d8c37042b46f453955db60cff602c67af5
Piotr Markiewicz <piotr.markiewicz@gmail.com>
2016-12-16 12:51:21 +0100

    parameter name changed

--- quicktest.py
+++ quicktest.py
@@ -5,30 +5,30 @@ from datetime import datetime
 
 import processor
 
-ipaddress = '192.168.10.150'
+host = '192.168.10.150'
 port = '8002'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Speedtest quicktest")
-    parser.add_argument('-a', '--address', help='Remote IP address')
+    parser.add_argument('-t', '--host', help='Remote host address')
     parser.add_argument('-p', '--port', help='Remote port')
 
     args = parser.parse_args()
 
-    if args.address:
-        ipaddress = args.address
+    if args.host:
+        host = args.host
 
     if args.port:
         port = args.port
 
-    slave = '{} {}'.format(ipaddress, port)
+    slave = '{} {}'.format(host, port)
     print slave
     #rc = redislite.StrictRedis(serverconfig={'slaveof': slave})
-    rc = redislite.StrictRedis(host = ipaddress, port = port)
+    rc = redislite.StrictRedis(host = host, port = port)
     lst = List(redis=rc, key='speed')
 
     print "Keys in redis: ", rc.keys()
-    print "No of items in redis: ", len(lst)
+    print "No of items in redis['speed']: ", len(lst)
     print "Weekly speed\n", processor.average_speed_weekly(lst)
 
     settings = Dict(redis=rc, key='settings')

179d68a05249578c20117e9a1827ccffdaaa98c7
Piotr Markiewicz <piotr.markiewicz@gmail.com>
2017-01-04 22:04:17 +0100

    logger configuration

---
+++ log.py
@@ -0,0 +1,18 @@
+import logging
+from os import path
+
+def get_logger(log_name, log_folder=None):
+    if not log_folder:
+        log_folder = path.dirname(path.abspath(__file__))
+
+    logger = logging.getLogger(log_name)
+
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s")
+
+    log_file = path.join(log_folder, log_name + '.pid')
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger

0767d2b14ffc2f0605d216f7b84ff7c23fde94dc
Piotr Markiewicz <piotr.markiewicz@gmail.com>
2017-01-04 22:25:39 +0100

    extension log file changed

--- log.py
+++ log.py
@@ -10,7 +10,8 @@ def get_logger(log_name, log_folder=None):
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s")
 
-    log_file = path.join(log_folder, log_name + '.pid')
+    log_file = path.join(log_folder, log_name + '.log')
+    print log_file
     handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
     logger.addHandler(handler)
